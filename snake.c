a141210dbd1f81cf8468e4e79d5202dc35f7463a
00wz <130168402+00wz@users.noreply.github.com>
2024-04-04 18:05:47 +0300

    homework

--- snake.c
+++ snake.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <conio.h>
 
 #define MAX_X 15
 #define MAX_Y 15
+
+enum DIRECTION {LEFT = -2, UP, NONE, DOWN, RIGHT};
 
 typedef struct tail_t
 {
@@ -58,11 +61,50 @@ void printSnake(struct snake_t snake)
         {
             printf("%c", matrix[i][j]);
         }
-            printf("\n");
+        printf("\n");
     }
 }
-	
-snake_t moveLeft(snake_t snake)
+
+snake_t move_head(snake_t snake, enum DIRECTION direction)
+{
+    switch (direction)
+    {
+    case LEFT:
+        snake.x--;
+        if (snake.x < 0)
+        {
+            snake.x = MAX_X - 1;
+        }
+        break;
+    
+    case RIGHT:
+        snake.x++;
+        if (snake.x >= MAX_X)
+        {
+            snake.x = 0;
+        }
+        break;
+    
+    case UP:
+        snake.y--;
+        if (snake.y < 0)
+        {
+            snake.y = MAX_Y - 1;
+        }
+        break;
+    
+    case DOWN:
+        snake.y++;
+        if (snake.y >= MAX_Y)
+        {
+            snake.y = 0;
+        }
+        break;
+    }
+	return snake;
+}	
+
+snake_t move(snake_t snake, enum DIRECTION direction)
 {
 	for (int i = snake.tsize - 1; i > 0; i--)
     {
@@ -71,22 +113,50 @@ snake_t moveLeft(snake_t snake)
 	snake.tail[0].x = snake.x;
 	snake.tail[0].y = snake.y;
 	
-	snake.x = snake.x - 1;	
-	if (snake.x < 0)
-    {
-		snake.x = MAX_X - 1;
-	}
-	return snake;
+    return move_head(snake, direction);
 }	
+	
+enum DIRECTION input()
+{
+	if ( !_kbhit() )
+	{
+		return NONE;
+	}
+	char key_code = getch();
+	switch (key_code)
+	{
+		case 'W':
+		case 'w':
+			return UP;
+		case 'S':
+		case 's':
+			return DOWN;
+		case 'A':
+		case 'a':
+			return LEFT;
+		case 'D':
+		case 'd':
+			return RIGHT;
+		default:
+			return NONE;
+	}
+}
 	
 int main()
 {
 	struct snake_t snake = initSnake( 10, 5, 2);
+    enum DIRECTION direction = DOWN;
+
 	printSnake(snake);
 	while(1)
 	{
-		snake = moveLeft(snake);
 		sleep(1);
+		enum DIRECTION new_dir = input();
+		if(new_dir != NONE && new_dir != -direction)
+		{
+			direction = new_dir;
+		}
+		snake = move(snake, direction);
 		system("cls");
 		printSnake(snake);
 	}
